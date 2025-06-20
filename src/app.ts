4949f590f759fc91d9a89a1067065fc403c3fc8d
rumon5h <mdrumonkhan48519@gmail.com>
2025-06-21 00:30:01 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { Application, Request, Response } from 'express';
+
+
+const app: Application = express();
+
+app.use(express.json())
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Welcome to the  Library Management Software!');
+});
+
+
+export default app;
