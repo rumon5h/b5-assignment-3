16c167b3c7ce42d580ebe5fdee450557c048884f
rumon5h <mdrumonkhan48519@gmail.com>
2025-06-21 15:28:29 +0600

    updated|

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,17 @@
 import { Server } from 'http';
 import mongoose from 'mongoose';
 import app from './app';
+import config from './config';
+
 
 let server: Server;
 
-const PORT = 3000;
+const PORT = config.port || 3000;
 
 async function main() {
     try {
-        await mongoose.connect('mongodb+srv://assignment_3:wxjoy4fn9Cb0MUys@cluster0.trt1vv0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+        await mongoose.connect(config.database_url as string);
+
         console.log("Connected to Database");
         server = app.listen(PORT, () => {
             console.log(`App is listening on port ${PORT}`);
