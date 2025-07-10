1303992a498c7ef7cc29bd5e976e7ec3268afe85
rumon5h <mdrumonkhan48519@gmail.com>
2025-07-11 00:19:06 +0600

    updated

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
 import express, { Application, Request, Response } from 'express';
 import { booksRouter } from './app/controllers/books.controller';
 import { borrowRouter } from './app/controllers/borrow.controller';
+import cors from "cors";
 
 const app: Application = express();
+
+
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://l2-b5-a4.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true
+}));
+
+app.use(express.json());
+
 
 app.use('/api/books', booksRouter);
 app.use('/api/borrow', borrowRouter);
