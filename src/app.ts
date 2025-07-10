08716841c297deb1082456aba9a6fc90ae7b892d
rumon5h <mdrumonkhan48519@gmail.com>
2025-07-11 00:01:00 +0600

    updated

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,12 @@
 import express, { Application, Request, Response } from 'express';
 import { booksRouter } from './app/controllers/books.controller';
 import { borrowRouter } from './app/controllers/borrow.controller';
-
-
+import cors from "cors";
 
 const app: Application = express();
 
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://l2-b5-a4.vercel.app"); // Update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-
 app.use(express.json());
+app.use(cors({origin: ["https://l2-b5-a4.vercel.app", "http://localhost:5173"]}))
 
 
 app.use('/api/books', booksRouter);
