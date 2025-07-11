462f936bc4576ef90cde34498fc92294386995fd
rumon5h <mdrumonkhan48519@gmail.com>
2025-07-11 10:28:17 +0600

    updated

--- src/server.ts
+++ src/server.ts
@@ -2,22 +2,8 @@ import { Server } from 'http';
 import mongoose from 'mongoose';
 import app from './app';
 import config from './config';
-import cors from "cors";
-import express from "express"
-
-
 let server: Server;
-
 const PORT = config.port || 3000;
-
-app.use(cors({
-  origin: ['http://localhost:5173', 'https://l2-b5-a4.vercel.app'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  credentials: true
-}));
-
-
-app.use(express.json());
 
 async function main() {
     try {
